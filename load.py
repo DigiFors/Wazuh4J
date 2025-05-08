760c59dad6d9fa43a59732b591ff4f008af329a8
nadja.ringleb <nadja.ringleb@digifors.de>
2025-05-08 12:40:25 +0200

    can now load xml files from multiple source folders

--- load.py
+++ load.py
@@ -1,27 +1,51 @@
-import requests
 import os
-import sys
+import shutil
+from pathlib import Path
+
+import click
 from neo4j import GraphDatabase
-import re
-from bs4 import BeautifulSoup
+
 
 # Neo4j connection details
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "CHANGE_ME_PLEASE"
+NEO4J_PASSWORD = "digiforss"
 
 FILE_SERVER_URL = "file://"
+XML_IMPORT_FOLDER = "./import"
 
-# Get XML file list from the directory listing
-def get_xml_files(path):
-    xml_files = []
-    for file_name in os.listdir(path):
-        if file_name.lower().endswith('.xml'):
-            xml_files.append(file_name)
-    return xml_files
 
-# Load XML files into Neo4j
+def copy_xml_files_and_get_paths(folder_paths) -> list:
+    """
+    Copy all XML files to "./import" and return a list of the file_path_obj copy paths.
+    :param folder_path: Path to the folders containing xml files
+    :return: A list of paths to the file_path_obj copies
+    """
+    xml_file_copies = []
+
+    # clear import folder
+    shutil.rmtree(XML_IMPORT_FOLDER)
+    os.makedirs(XML_IMPORT_FOLDER)
+
+    for folder in folder_paths:
+        # find all xml files in the folder
+        xml_file_path_objs = [x.resolve() for x in Path(folder).rglob("*.xml")]
+        for file_path_obj in xml_file_path_objs:
+            # copy file
+            print(f"Copying {str(file_path_obj)} ...")
+            file_copy_path = shutil.copy(file_path_obj, Path(XML_IMPORT_FOLDER, file_path_obj.name))
+            # store path to file copy
+            xml_file_copies.append(str(Path(file_copy_path).as_posix()))
+
+    return xml_file_copies
+
+
 def load_files_into_neo4j(xml_files):
+    """
+    Load XML files into Neo4j
+    :param xml_files:
+    :return:
+    """
     with GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD)) as driver:
         with driver.session() as session:
             for xml_file in xml_files:
@@ -57,22 +81,24 @@ def load_files_into_neo4j(xml_files):
                 """
                 try:
                     session.run(cypher_query, xml_url=xml_url)
-                    print(f"Loaded {xml_file} into Neo4j.")
+                    print(f"Loaded {xml_url} into Neo4j.")
                 except Exception as e:
-                    print("YOOO ERROR")
-                    print(e)
+                    print(f"Could not load xml file to Neo4j db: {e}")
 
 
-# Main Execution
-def main():
-    p = sys.argv[1]
-    xml_files = get_xml_files(p)
-    print("loading these files")
-    print(xml_files)
+@click.command()
+@click.option('--xml-folders', '-x', multiple=True, type=click.Path(exists=True, readable=True))
+def main(xml_folders):
+    print(f"Searching for XML files in {xml_folders}")
+    xml_files = copy_xml_files_and_get_paths(xml_folders)
+
     if not xml_files:
         print("No XML files found.")
         return
+
+    print("Loading files ino database:")
     load_files_into_neo4j(xml_files)
+
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Wazuh4j
-This repository contains everything necessary for 1) importing 2) visualizing and 3) analysing the wazuh regelset.  
+This repository contains everything necessary for 1) importing 2) visualizing and 3) analysing the wazuh rule set.  
 
 ## neo4j
 the database behind all this is neo4j. it is a graph database perfectly tailored to this problem. more information about it here: 
@@ -10,14 +10,13 @@ https://medium.com/@balajeraam/neo4j-for-beginners-a8e5a64b074a
 ## usage
 1) start the docker container `docker compose up`
 
-    1.1) change the password for neo4j (visit http://ipaddress:7474 , creds `neo4j:neo4j`) 
+    1.1) change the password for neo4j (visit http://localhost:7474 , creds `neo4j:neo4j`) 
     
     1.2) put the new password into the python file (`NEO4J_PASSWORD` quick and dirty FOR NOW!)
-    
-2) put wazuh rules into the `./import` folder 
-3) initiate python `python3 -m venv venv` and `pip3 install -r requirements.txt` 
-4) call python script ON THE SAME HOST AS DOCKER: `python3 load.py <path_to_import_folder>` 
-5) profit 
+
+2) Initiate a python virtual environment with `pipenv shell` and install dependencies with `pipenv update` 
+3) To load wazuh rules into the database, run the python script ON THE SAME HOST AS THE DOCKER CONTAINER: `python3 load.py -x <path_to_folder_with_xml_files>`. If you want to load xml files from multiple folders just add ` -x <another_folder_path` for each folder. 
+4) profit 
 
 
 use this query for testing: ` match p=(:Rule {id: "101527"})-[:DEPENDS_ON * ]->(:Rule) return p ; `
@@ -35,5 +34,5 @@ here are some queries which are commonly used and their SQL equivalents.
 | `match (n:LABEL) return n.attr1, n.attr2 ;` | `select attr1,attr2 from LABEL ; ` | get fields of a certain type                          |
 
 
-here is some general introduciton into neo4j 
+here is some general introduction into neo4j 
 https://medium.com/@balajeraam/neo4j-for-beginners-a8e5a64b074a
